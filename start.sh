45a8e384da40c8b751b836238de8e11dade73d8c
GrandMEAN-RA <grandmeanra@gmail.com>
2025-10-10 14:02:01 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "🚀 Starting Iya Bola Assistant App..."
-streamlit run Banking_Iya_Bola_App.py --server.port=$PORT --server.address=0.0.0.0
+streamlit run Banking_Iya_Bola_App_Full.py --server.port=$PORT --server.address=0.0.0.0
+
